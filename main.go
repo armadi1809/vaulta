923fd2f2feed988cc44b3f13bd4e8356bdeb34df
Aziz Rmadi <azizrmadi@gmail.com>
2026-01-08 18:01:25 +0100

    skeleton for init function

--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"os"
+
+	"golang.org/x/crypto/argon2"
 )
 
 const usage string = `Usage: vaulta <command> <parameters>
 Available Commands: 
 - init: Initialize a vault if none exists on your machine`
+
+type VaultFile struct {
+	Version int       `json:"version"`
+	KDF     KDFConfig `json:"kdf"`
+	Cipher  Cipher    `json:"cipher"`
+}
+
+type KDFConfig struct {
+	Algorithm   string `json:"algorithm"`
+	Salt        string `json:"salt"`
+	Iterations  uint32 `json:"iterations"`
+	Memory      uint32 `json:"memory"`
+	Parallelism uint8  `json:"parallelism"`
+}
+
+type Cipher struct {
+	Algorithm string `json:"algorithm"`
+	Nonce     string `json:"nonce"`
+	Data      string `json:"data"`
+}
 
 func main() {
 	args := os.Args
@@ -26,4 +53,68 @@ func main() {
 		// delete entry workflow
 	}
 
+}
+
+func initVault(path string) error {
+	var masterPwd string
+	fmt.Print("Choose a master password: \n")
+	fmt.Scanln(&masterPwd)
+
+	salt, err := randomBytes(16)
+	if err != nil {
+		return err
+	}
+
+	key := argon2.IDKey([]byte(masterPwd), salt, 3, 64*1024, 2, 32)
+
+	plaintext := []byte(`{"entries":[]}`)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	nonce, err := randomBytes(gcm.NonceSize())
+	ciphertext := gcm.Seal(nil, nonce, plaintext, nil)
+
+	vault := VaultFile{
+		Version: 1,
+		KDF: KDFConfig{
+			Algorithm:   "argon2id",
+			Salt:        base64.StdEncoding.EncodeToString(salt),
+			Iterations:  3,
+			Memory:      64 * 1024,
+			Parallelism: 2,
+		},
+		Cipher: Cipher{
+			Algorithm: "aes-256-gcm",
+			Nonce:     base64.StdEncoding.EncodeToString(nonce),
+			Data:      base64.StdEncoding.EncodeToString(ciphertext),
+		},
+	}
+
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+	enc := json.NewEncoder(file)
+	enc.SetIndent("", "  ")
+	return enc.Encode(vault)
+
+}
+
+func randomBytes(size int) ([]byte, error) {
+	buf := make([]byte, size)
+	_, err := rand.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+	return buf, nil
 }
